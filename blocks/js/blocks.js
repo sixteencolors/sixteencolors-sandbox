1ddac21e53897358e6dcadd2e6577cc19d7ee012
Todd Boyd <haliphax@gmail.com>
2013-09-09 08:07:52 -0500

    erased trailing whitespace

--- blocks/js/blocks.js
+++ blocks/js/blocks.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
 		$('html').css({ overflow: 'hidden' });
 	}).bind('cbox_closed', function () {
 		$('html').css({ overflow: 'auto' });
-	}); 
+	});
 
 	/**
 	 * When scrolled all the way to the bottom, add more tiles.
@@ -58,7 +58,7 @@ $(document).ready(function () {
 
 	function applyLayout() {
 		clearProgress();
-	
+
 		options.container.imagesLoaded(function (instance) {
 			$('#progress').hide();
 			$('#pack_contents ul').css('left', 'auto');
@@ -67,18 +67,18 @@ $(document).ready(function () {
 				itemSelector: 'li:not(.stamp)'
 				, stamp: '.stamp'
 				, gutter: 16
-			});  
-	
+			});
+
 			var msnry = options.container.data('masonry');
-	
+
 			$('#pack_contents ul').css("text-indent", "auto");
 			// Create a new layout handler when images have loaded.
 			handler = $('#pack_contents ul li');
-	
+
 			for (var i = 0, len = instance.images.length; i < len; i++ ) {
 				var image = instance.images[i];
 			}
-	
+
 			$('a', handler).colorbox({
 				rel: 'lightbox'
 				, photo: true
@@ -89,27 +89,27 @@ $(document).ready(function () {
 					$('#cboxTitle').html($(this).find('label span').html());
 				}
 			});
-	
+
 			msnry.layout();
 		})
 		.progress(function(instance, image) {
 			if (image.isLoaded) {
 				$('<span>.</span>').appendTo('#progress')
 			}
-			
+
 			var result = image.isLoaded? 'loaded' : 'broken';
 		});
-	
+
 	}
-	
+
 	function clearProgress() {
 		$('#progress').text('Loading');
 	}
-	
+
 	function loadData() {
 		isLoading = true;
 		$('#loaderCircle').show();
-	
+
 		$.ajax({
 			url: apiURL + 'pack/' + pack,
 			dataType: 'jsonp',
@@ -117,7 +117,7 @@ $(document).ready(function () {
 			success: onLoadPack
 		});
 	}
-	
+
 	function generatePackHtml(data) {
 		var html = '<h1>' + data.name + ', ' + data.year + '</h1>';
 		html += '<div id="progress">Loading</div><ul class="pack">';
@@ -132,36 +132,36 @@ $(document).ready(function () {
 		for (; i < length; i++) {
 			image = data.files[i];
 			cssClass = "";
-		
+
 			if (/\.bin/i.test(image.filename)) {
 				cssClass = 'bin';
-			} 
-		
+			}
+
 			html += '<li class="' + cssClass + '"><a href="http://sixteencolors.net' + image.fullsize + '" rel="lightbox">';
 			html += '<img src="http://sixteencolors.net' + image.thumbnail + '" alt="' + image.filename + '" />';
 			html += '<label><span>' + image.filename + '</span></label>';
 			html += '</a></li>';
 		}
-	
+
 		html += '</ul>';
-	
+
 		// Add image HTML to the page.
 		$('#pack_contents').append(html);
-	
+
 		// Apply layout.
 		applyLayout();
 	}
-	
+
 	/**
 	 * Receives data from the API, creates HTML for images and updates the layout
 	 */
-	
+
 	function onLoadPack(data) {
 		isLoading = false;
 		$('#loaderCircle').hide();
 		generatePackHtml(data);
 	}
-	
+
 	// Capture scroll event.
 	// $(document).bind('scroll', onScroll);
 
